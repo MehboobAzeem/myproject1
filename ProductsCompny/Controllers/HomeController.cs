44d3bc73c8e59be91ef26cf9a21394d4becf9d19
MehboobAzeem <30482100+MehboobAzeem@users.noreply.github.com>
2017-08-05 01:54:58 +0500

    myproject

---
+++ ProductsCompny/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductsCompny.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductCompnyEntities db = new ProductCompnyEntities();
+
+        public ActionResult Index()
+        {
+            var pro = db.Products.ToList();
+            ViewBag.prod = pro;
+            return View();
+        }
+
+
+
+      
+    }
+}
